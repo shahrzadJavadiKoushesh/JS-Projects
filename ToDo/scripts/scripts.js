69b88d94d4a1f26ebe980be2bfd3e4410c1ce120
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-08-10 11:46:00 +0430

    type sth alert

--- ToDo/scripts/scripts.js
+++ ToDo/scripts/scripts.js
@@ -1,11 +1,15 @@
-let myform = document.querySelector('#myform');
-
 let todotext = document.querySelector('.todo');
 
+let myform = document.querySelector('#myform');
+
 myform.addEventListener('submit', function(){
-    let listitem = document.createElement('li');
-    listitem.innerHTML = todotext.value;
-    let taskslist = document.querySelector('#taskslist ul');
-    taskslist.appendChild(listitem);
+    if(todotext.value != ''){
+        let listitem = document.createElement('li');
+        listitem.innerHTML = todotext.value;
+        let taskslist = document.querySelector('#taskslist ul');
+        taskslist.appendChild(listitem);
+    }else{
+        alert('You have to type something!');
+    }
 });
 
