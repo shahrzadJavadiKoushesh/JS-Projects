7c47d9952d4151d47e18d2d231492a8639169873
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-08-19 20:22:07 +0430

    delete icon added

--- ToDo/scripts/scripts.js
+++ ToDo/scripts/scripts.js
@@ -4,6 +4,8 @@ let myform = document.querySelector('#myform');
 
 let numberOfTasks = 0;
 
+// let myul = document.querySelector('#myUl');
+
 myform.addEventListener('submit', function(){
     if(todotext.value != ''){
         let listitem = document.createElement('li');
@@ -13,6 +15,18 @@ myform.addEventListener('submit', function(){
         todotext.value = '';
         numberOfTasks++;
         document.querySelector('#totaltasks').innerHTML = numberOfTasks;
+        // remove item 
+        let removeList = document.createElement('img');
+        listitem.appendChild(removeList);
+        removeList.setAttribute('src', 'assets/icons8-remove-24.png');
+        removeList.setAttribute('title', 'delete');
+        removeList.setAttribute('alt', 'trash');
+
+        removeList.addEventListener('click', function(){
+            taskslist.removeChild(listitem);
+            numberOfTasks--;
+            document.querySelector('#totaltasks').innerHTML = numberOfTasks;
+        });
     }else{
         alert('You have to type something!');
     }
