3bb056304483abeeca55c432dbe121e61c945a2b
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-09-01 13:33:07 +0430

    eye image removed when input is empty

--- Login Page/scripts/scripts.js
+++ Login Page/scripts/scripts.js
@@ -1,6 +1,16 @@
+// eye image 
 let eyeimg = document.querySelector('.pass-visibility');
-
+// password input 
 let passInput = document.querySelector('#password'); 
+
+passInput.addEventListener('keyup',function(){
+    let passInputLength = passInput.value.length;
+    if (passInputLength == 0){
+        eyeimg.style.display = 'none';
+    }else{
+        eyeimg.style.display = 'inline-block';
+    }
+});
 
 eyeimg.addEventListener('click', function(){
     let passInputType = passInput.getAttribute('type');
