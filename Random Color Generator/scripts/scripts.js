58097405198f65b10e2f0b83752af33bb2915918
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-10-26 11:51:26 +0330

    getting color range

--- Random Color Generator/scripts/scripts.js
+++ Random Color Generator/scripts/scripts.js
@@ -1,1 +1,23 @@
-alert('HI')
+let rRange = document.querySelector('#r-range');
+let gRange = document.querySelector('#g-range');
+let bRange = document.querySelector('#b-range');
+rRange.addEventListener('input', function() {
+    console.log(rRange.value);
+});
+
+gRange.addEventListener('input', () => {
+    console.log(gRange.value);
+    
+});
+
+bRange.addEventListener('input', () => {
+    console.log(bRange.value);
+});
+
+let randombtn = document.querySelector('#random-btn');
+
+let rangesValues = 'RGB is: ' + rRange.value + ", " + gRange.value + ", " + bRange.value;
+
+randombtn.addEventListener('click', () =>{
+    alert(rangesValues);
+});
