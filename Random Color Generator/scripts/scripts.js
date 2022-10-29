edb67045abec9d56a7349b48f7113d3103ec6ef6
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-10-29 11:56:37 +0330

    changes for getting number of colors as alert

--- Random Color Generator/scripts/scripts.js
+++ Random Color Generator/scripts/scripts.js
@@ -1,23 +1,25 @@
 let rRange = document.querySelector('#r-range');
 let gRange = document.querySelector('#g-range');
 let bRange = document.querySelector('#b-range');
+let randombtn = document.querySelector('#random-btn');
+
 rRange.addEventListener('input', function() {
-    console.log(rRange.value);
+    // console.log(rRange.value);
+    let red_value = rRange.value;
+    let green_value = gRange.value;
+    let blue_value = bRange.value;
 });
 
-gRange.addEventListener('input', () => {
+gRange.addEventListener('input', function() {
     console.log(gRange.value);
-    
 });
 
-bRange.addEventListener('input', () => {
+bRange.addEventListener('input', function() {
     console.log(bRange.value);
 });
 
-let randombtn = document.querySelector('#random-btn');
 
+randombtn.addEventListener('click', function(){
 let rangesValues = 'RGB is: ' + rRange.value + ", " + gRange.value + ", " + bRange.value;
-
-randombtn.addEventListener('click', () =>{
     alert(rangesValues);
 });
