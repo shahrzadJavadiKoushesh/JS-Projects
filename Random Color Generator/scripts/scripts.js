f30587e9eec7387b6e73c559bc096be085c6ae0c
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-10-30 10:01:05 +0330

    backgroundcolor change with input change

--- Random Color Generator/scripts/scripts.js
+++ Random Color Generator/scripts/scripts.js
@@ -2,20 +2,27 @@ let rRange = document.querySelector('#r-range');
 let gRange = document.querySelector('#g-range');
 let bRange = document.querySelector('#b-range');
 let randombtn = document.querySelector('#random-btn');
+let bodyTag = document.querySelector('body');
 
 rRange.addEventListener('input', function() {
-    // console.log(rRange.value);
     let red_value = rRange.value;
     let green_value = gRange.value;
     let blue_value = bRange.value;
+    bodyTag.style.backgroundColor = "rgb("+ red_value +", "+ green_value +", "+blue_value+")";
 });
 
 gRange.addEventListener('input', function() {
-    console.log(gRange.value);
+    let red_value = rRange.value;
+    let green_value = gRange.value;
+    let blue_value = bRange.value;
+    bodyTag.style.backgroundColor = "rgb("+ red_value +", "+ green_value +", "+blue_value+")";
 });
 
 bRange.addEventListener('input', function() {
-    console.log(bRange.value);
+    let red_value = rRange.value;
+    let green_value = gRange.value;
+    let blue_value = bRange.value;
+    bodyTag.style.backgroundColor = "rgb("+ red_value +", "+ green_value +", "+blue_value+")";
 });
 
 
