bb5235981a6eb843a704528c35f84bb239647ed9
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-11-02 10:26:14 +0330

    small func changes

--- Random Color Generator/scripts/scripts.js
+++ Random Color Generator/scripts/scripts.js
@@ -4,27 +4,15 @@ let bRange = document.querySelector('#b-range');
 let randombtn = document.querySelector('#random-btn');
 let bodyTag = document.querySelector('body');
 
-rRange.addEventListener('input', function() {
-    let red_value = rRange.value;
-    let green_value = gRange.value;
-    let blue_value = bRange.value;
-    bodyTag.style.backgroundColor = "rgb("+ red_value +", "+ green_value +", "+blue_value+")";
-});
+function change_color (){
+    bodyTag.style.backgroundColor = "rgb("+ rRange.value +", "+ gRange.value +", "+bRange.value+")";
+}
 
-gRange.addEventListener('input', function() {
-    let red_value = rRange.value;
-    let green_value = gRange.value;
-    let blue_value = bRange.value;
-    bodyTag.style.backgroundColor = "rgb("+ red_value +", "+ green_value +", "+blue_value+")";
-});
+rRange.addEventListener('input', change_color());
 
-bRange.addEventListener('input', function() {
-    let red_value = rRange.value;
-    let green_value = gRange.value;
-    let blue_value = bRange.value;
-    bodyTag.style.backgroundColor = "rgb("+ red_value +", "+ green_value +", "+blue_value+")";
-});
+gRange.addEventListener('input', change_color());
 
+bRange.addEventListener('input', change_color());
 
 randombtn.addEventListener('click', function(){
 let randomNumRed = Math.floor(Math.random() * 255);
