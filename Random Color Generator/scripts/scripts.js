de2034297dd0e10c26ad14873d5ca2c853f6bc6b
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-11-05 11:52:10 +0330

    copy by range

--- Random Color Generator/scripts/scripts.js
+++ Random Color Generator/scripts/scripts.js
@@ -6,14 +6,20 @@ let bodyTag = document.querySelector('body');
 
 function change_color (){
     bodyTag.style.backgroundColor = "rgb("+ rRange.value +", "+ gRange.value +", "+bRange.value+")";
+    const textarea = document.createElement("textarea");
+    textarea.value =rRange.value + ', ' + gRange.value+ ', ' + bRange.value;
+    bodyTag.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
     document.querySelector('#red').innerHTML = rRange.value;
     document.querySelector('#green').innerHTML = gRange.value;
     document.querySelector('#blue').innerHTML = bRange.value;
 }
 
 rRange.addEventListener('input',function(){
-    change_color()}
-);
+    change_color();
+});
 
 gRange.addEventListener('input', function(){
 change_color()}
@@ -32,12 +38,12 @@ rRange.value = randomNumRed;
 gRange.value = randomNumGreen;
 bRange.value = randomNumBlue;
 const textarea = document.createElement("textarea");
-textarea.value =randomNumRed + ', ' + randomNumGreen + ', ' + randomNumBlue;
+textarea.value =rRange.value + ', ' + gRange.value+ ', ' + bRange.value;
 document.body.appendChild(textarea);
     textarea.select();
     document.execCommand('copy');
     textarea.remove();
-    document.querySelector('#red').innerHTML = randomNumRed;
-    document.querySelector('#green').innerHTML = randomNumGreen;
-    document.querySelector('#red').innerHTML = randomNumBlue;
-});
+    document.querySelector('#red').innerHTML = rRange.value;
+    document.querySelector('#green').innerHTML = gRange.value;
+    document.querySelector('#blue').innerHTML = bRange.value;
+}); 
