3bed59ee753d3493fc576bac76bbc4cc2d6ff8cc
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-12-12 13:43:43 +0330

    list of products added

---
+++ cart/scripts/products.js
@@ -0,0 +1,74 @@
+let products = [
+    {
+        id:0,
+        name: "کفش مردانه چرم",
+        price: 162000,
+        instock: 14,
+        imgSrc: "images/1.jpg"
+    },
+
+    {
+        id:1,
+        name: "عروسک اسباب بازی",
+        price: 12000,
+        instock: 2,
+        imgSrc: "images/2.jpg"
+    },
+
+    {
+        id:2,
+        name: "سرویس قابلمه",
+        price: 28000,
+        instock: 20,
+        imgSrc: "images/3.jpg"
+    },
+
+    {
+        id:3,
+        name: "هدست گیمینگ",
+        price: 2900000,
+        instock: 3,
+        imgSrc: "images/4.jpg"
+    },
+
+    {
+        id:4,
+        name: "دستگاه سنگ تراش",
+        price: 1340000,
+        instock: 7,
+        imgSrc: "images/5.jpg"
+    },
+
+    {
+        id:5,
+        name: "مک بوک اپل",
+        price: 70000000,
+        instock: 5,
+        imgSrc: "images/6.jpg"
+    },
+
+    {
+        id:6,
+        name: "دریل + ابزار",
+        price: 340000,
+        instock: 11,
+        imgSrc: "images/7.jpg"
+    },
+
+    {
+        id:7,
+        name: "اسپیکر بلوتوثی",
+        price: 1200000,
+        instock: 4,
+        imgSrc: "images/8.jpg"
+    },
+
+    {
+        id:8,
+        name: "کفش بوت چرم",
+        price: 197000,
+        instock: 4,
+        imgSrc: "images/1.jpg"
+    },
+
+];
