addbd9873e695c460b088ddb1c907bdd5ed3aac8
Shahrzad <shahrzadjkoushesh@gmail.com>
2022-12-13 19:31:06 +0330

    divide nums by three

--- cart/scripts/scripts.js
+++ cart/scripts/scripts.js
@@ -12,7 +12,7 @@ function showAllProducts(){
         <div class="product-title">`+products[i].name+`</div>
         <div class="product-instock">تعداد موجود: ` + products[i].instock + `</div>
         <div class="product-data">
-            <div class="product-price">`+products[i].price+`</div>
+            <div class="product-price">`+commafy(products[i].price)+`</div>
             <div class="product-addToCart" onclick="add_to_cart(`+products[i].id+`)"><i class="fa-solid fa-cart-shopping"></i></div>
         </div>
     </div>`;
@@ -62,7 +62,7 @@ function render_cart_items(){
     for(let i = 0; i < cart.length; i++){
         cart_items.innerHTML += `<li id="cart-item">
         <div id="p-name" onclick="deleteItem(`+cart[i].id+`)">`+cart[i].name+`</div>
-        <div id="p-price">`+cart[i].price+`</div>
+        <div id="p-price">`+commafy(cart[i].price)+`</div>
          <div id="p-unit">
             <span class="plus" onclick="change_numbers_of_items('plus', `+ cart[i].id +`)"><i class="fa-solid fa-plus"></i></span>
             <span class="unit">`+cart[i].number_of_units+`</span>
@@ -102,7 +102,7 @@ function renderTotal(){
     }
 
     total_item_elements.innerHTML = totalItems;
-    total_price_elements.innerHTML = totalPrice;
+    total_price_elements.innerHTML = commafy(totalPrice);
 }
 
 // delete item from cart
@@ -111,4 +111,15 @@ function deleteItem(id){
         return item.id != id;
     });
     render_cart_items();
+}
+
+function commafy(num) {
+    var str = num.toString().split('.');
+    if (str[0].length >= 5) {
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1.');
+    }
+    if (str[1] && str[1].length >= 5) {
+        str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return str.join('.');
 }
